d66731574d934bf4fb76b45f15d84cff6f8141b2
EqualRanc <92188554+EqualRanc@users.noreply.github.com>
2022-05-11 16:25:03 -0700

    Update AssayScreeningDataProcessor_v7.py

--- AssayScreeningDataProcessor_v7.py
+++ AssayScreeningDataProcessor_v7.py
@@ -2,6 +2,8 @@
 #"""
 #This script is intended to automatically process raw UNMODIFIED .csv files read from the Envision
 # to create a DB upload file.
+#
+# author: EqualRanc
 #    
 #The second part of this script is designed to handle and process data from SP screens.
 
@@ -128,24 +130,130 @@ def normalize1536(foldername):
     normalizedcoldata = [j for i in normalizedcoldata for j in i]
     return normalizedcoldata, hisdata, losdata, hiadata, loadata, zdata, windowdata
 
+def uinput():
+    initialcheckboxlist = []
+    if values['-SS-'] == True:
+        initialcheckboxlist.append(["data 1", "data 2", "data 3"])
+    if values['-S-'] == True:
+        initialcheckboxlist.append(["data 4"])
+    if values['-NN-'] == True:
+        initialcheckboxlist.append(["data 5", "data 6", "data 7", "data 8"])
+    if values['-N-'] == True:
+        initialcheckboxlist.append(["data 9", "data 10", "data 11", "data 12"])
+    if values['-O-'] == True:
+        initialcheckboxlist.append(["data 13", "data 14"])
+    if values['-AT-'] == True:
+        initialcheckboxlist.append(["data 15"])
+    if values['-CA-'] == True:
+        initialcheckboxlist.append(["data 16", "data 17", "data 18", "data 19", "data 20", "data 21", "data 22", "data 23"])
+    if values['-AH-'] == True:
+        initialcheckboxlist.append(["data 24"])
+    if values['-KT-'] == True:
+        initialcheckboxlist.append(["data 25"])
+    if values['-W-'] == True:
+        initialcheckboxlist.append(["data 26", "data 27"])
+    if values['-ORPH-'] == True:
+        initialcheckboxlist.append(["data 99"])
 
-       
-#Define checkboxes for fragment classes
-monocheckboxes = [[sg.Checkbox(':XA', default=False, key="-NN-"), sg.Checkbox(':XB', default=False, key="-N-"),
-    sg.Checkbox(':1Z', default=False, key="-S-"), sg.Checkbox(':Z', default=False, key="-SS-"),
-    sg.Checkbox(':O', default=False, key="-O-"), sg.Checkbox(':Q', default=False, key="-AT-"),
-    sg.Checkbox(':ZO', default=False, key="-CA-"), sg.Checkbox(':TGTT', default=False, key="-AH-"),
-    sg.Checkbox(':BR', default=False, key="-KT-"), sg.Checkbox(':W', default=False, key="-W-"), sg.Checkbox(':ID', default=False, key="-ORPH-")]
-]
+    initialcheckboxlist = [j for i in initialcheckboxlist for j in i]
+    if len(initialcheckboxlist) == 0:
+        window['assaystatus'].print("Please pick at least one fragment class (Assay Tab).")
+        
+    #Filters out the data tabs you'd like to exclude from the assaycheckboxlist
+    if len(values['-excl-']) != 0:
+        exclinput = values['-excl-']
+        
+        #Converts user inputs into the tab names
+        excl = ["data " + str(xin) for xin in exclinput]
+        
+        #Format for this line is [expression for item in iterable if condition == True]
+        assaycheckboxlist = []
+        assaycheckboxlist = [x1 for x1 in initialcheckboxlist if not any(x2 == x1 for x2 in excl)]
+    assaycheckboxlist = initialcheckboxlist
+    return assaycheckboxlist
 
-#Setup for the singple-point data processing main interface
-dataentries = [
-     [sg.Text("Single-Point Assay Data Processing Tool",font='Any 18')],
-     [sg.Frame('Browse to raw screening data folder:', [[sg.Input(key='-rawdata-'), sg.FolderBrowse(target='-rawdata-')]])],
-     [sg.Frame('Browse to the chemical database file:', [[sg.Input(key='-oner-'), sg.FileBrowse(target='-oner-')]])],
-     [sg.Frame("Please select desired assay plate type:", [[sg.Radio('1536W Assay', "AssayType", default=False, key="-A1536-"), sg.Radio('384W Assay', "AssayType", default=False, key="-A384-")]])],
-     [sg.Frame("Choose among the following classes:", monocheckboxes)],
-     [sg.Frame("Enter metadata, e.g. xb ID, LL type, assay info, target, assay concentration:",
+def dfilter(oner, assaycheckboxlist):
+    assayfullsheet = pd.concat(oner, keys=assaycheckboxlist)
+    assayslice = assayfullsheet.loc[:, [
+        'Class',
+        'Molecule Name',
+        'Batch Name',
+        'Batch External Identifier',
+        'Storage 96W or Box ID',
+        'Well',
+        'Concentration (mM)',
+        '384W ML',
+        '384W ML Well',
+        '1536W ML',
+        '1536W ML Well',
+        '1536W LL',
+        '1536W LL Well',
+        '1536W ZI',
+        '1536W ZI Well',
+        '384W ZI',
+        '384W ZI Well']]
+    assayslice['Target'] = values['-target-']
+    assayslice['Run Date'] = values['-rundate-']
+    assayslice['Run ID'] = values['-runid-']
+    assayslice['xb ID'] = values['-xbid-']
+    assayslice['Assay ID'] = values['-assayid-']
+    assayslice['Concentration (uM)'] = values['-conc-']
+    return assayslice
+
+def psummary(assayslice):
+    apnames = []
+    if values['-A1536-'] == True:
+        assayslice = assayslice[(~assayslice['Molecule Name'].isin(['XB-01', 'BB8900', 'EC57', 'EC98']))]
+        datalist, hisdata, losdata, hiadata, loadata, zdata, windowdata = normalize1536(foldername)
+        assayslice['% Activity (DMSO) plate'] = datalist
+        apnamestemp = []
+        for tab in assaycheckboxlist:
+            apnamestemp.append(oner[tab].loc[0, ['1536W ZI']])
+        for series in apnamestemp:
+            apnames.append(series[0])
+        assayslice = assayslice[(assayslice['Class'] != 'empty')] #Cleans up DB upload sheet
+    if values['-A384-'] == True:
+        assayslice = assayslice[(~assayslice['Molecule Name'].isin(['xb', 'BB8900', 'EC57', 'EC98']))]
+        datalist, hisdata, losdata, hiadata, loadata, zdata, windowdata = normalize384(foldername)
+        assayslice['% Activity (DMSO) plate'] = datalist
+        for tab in assaycheckboxlist:
+            apnamestemp.append(oner[tab].loc[0, ['384W ZI']])
+        for series in apnamestemp:
+            apnames.append(series[0])
+        assayslice = assayslice[(assayslice['Class'] != 'empty')] #Cleans up DB upload sheet
+    return assayslice, apnames, datalist, hisdata, losdata, hiadata, loadata, zdata, windowdata
+
+def pexcel():
+    psheaders = ["Plate Name","Fold Window","Z'","High Avg.","High Std. Dev.","Low Avg.","Low Std. Dev."]
+    ps = xw.Book()
+    pssheet = ps.sheets[0]
+    pssheet.range('A1').value = psheaders
+    pssheet.range('A2').options(transpose=True).value = apnames
+    pssheet.range('B2').options(transpose=True).value = windowdata
+    pssheet.range('C2').options(transpose=True).value = zdata
+    pssheet.range('D2').options(transpose=True).value = hiadata
+    pssheet.range('E2').options(transpose=True).value = hisdata
+    pssheet.range('F2').options(transpose=True).value = loadata
+    pssheet.range('G2').options(transpose=True).value = losdata
+    return ps
+
+def datalayout():            
+    #Define checkboxes for fragment classes
+    monocheckboxes = [[sg.Checkbox(':XA', default=False, key="-NN-"), sg.Checkbox(':XB', default=False, key="-N-"),
+                       sg.Checkbox(':1Z', default=False, key="-S-"), sg.Checkbox(':Z', default=False, key="-SS-"),
+                       sg.Checkbox(':O', default=False, key="-O-"), sg.Checkbox(':Q', default=False, key="-AT-"),
+                       sg.Checkbox(':ZO', default=False, key="-CA-"), sg.Checkbox(':TGTT', default=False, key="-AH-"),
+                       sg.Checkbox(':BR', default=False, key="-KT-"), sg.Checkbox(':W', default=False, key="-W-"), sg.Checkbox(':ID', default=False, key="-ORPH-")]
+    ]
+
+    #Setup for the singple-point data processing main interface
+    dataentries = [
+        [sg.Text("Single-Point Assay Data Processing Tool",font='Any 18')],
+        [sg.Frame('Browse to raw screening data folder:', [[sg.Input(key='-rawdata-'), sg.FolderBrowse(target='-rawdata-')]])],
+        [sg.Frame('Browse to the chemical database file:', [[sg.Input(key='-oner-'), sg.FileBrowse(target='-oner-')]])],
+        [sg.Frame("Please select desired assay plate type:", [[sg.Radio('1536W Assay', "AssayType", default=False, key="-A1536-"), sg.Radio('384W Assay', "AssayType", default=False, key="-A384-")]])],
+        [sg.Frame("Choose among the following classes:", monocheckboxes)],
+        [sg.Frame("Enter metadata, e.g. xb ID, LL type, assay info, target, assay concentration:",
                [
                    [
                        sg.Text("Run Date:"),sg.Input(key="-rundate-"), sg.Text("Run ID:"),sg.Input(key="-runid-")],
@@ -155,39 +263,41 @@ dataentries = [
                        sg.Text("Assay ID:"),sg.Input(key="-assayid-"), sg.Text("Concentration (uM):"),sg.Input(key="-conc-")]
                ]
               )
-      ],
-     [sg.Frame("Enter any assay plate numbers you'd like to exclude:", [[sg.Input(key='-excl-')]])],
-     [sg.Submit(), sg.Cancel()]
-]
+         ],
+        [sg.Frame("Enter any assay plate numbers you'd like to exclude:", [[sg.Input(key='-excl-')]])],
+        [sg.Submit(), sg.Cancel()]
+    ]
 
 
-datastatus = [
-    [sg.Text('Status:', size=[20,1])],
-     [sg.Multiline(key='datastatus',autoscroll=True,size=(30,20))],
-]
+    datastatus = [
+        [sg.Text('Status:', size=[20,1])],
+        [sg.Multiline(key='datastatus',autoscroll=True,size=(30,20))],
+    ]
 
-datalayout = [
-    [
-     sg.Column(dataentries),
-     sg.VSeperator(),
-     sg.Column(datastatus)
-     ]
-]
+    datalayout = [
+        [
+        sg.Column(dataentries),
+        sg.VSeperator(),
+        sg.Column(datastatus)
+        ]
+    ]
+    return datalayout
 
-#Creates the theme
+# Creates the theme
 sg.theme('Dark Teal')
+datalayout = datalayout()
 
-#Creates the window
+# Creates the window
 window = sg.Window('Single-Point Assay Data Processing Tool', datalayout, no_titlebar=False, alpha_channel=.9, grab_anywhere=True)
 
 
-#Create event loop to enable user inputs
+# Create event loop to enable user inputs
 while True:
     event, values = window.read()
     if event in (sg.WINDOW_CLOSED, "Cancel"):
         break
     
-    #Processing steps for renaming assay plates tab
+    # Processing steps for renaming assay plates tab
     elif event == "Submit":
         try:
             foldername = values['-rawdata-']
@@ -206,109 +316,18 @@ while True:
             oner = process_xl(27, cdb)
             
 #~+*^*+~+*^*+~+*^*+~+*^*+~+*^*+~+*^*+~+*^*+~+*^*+~+*^*+~+*^*+~+*^*+~+*^*+~+*^*+~+*^*+~+*^*+~+*^*+~+*^*+~+*^*+~+*^*+~+*^*+~+*^*+~            
-            initialcheckboxlist = []
-            if values['-SS-'] == True:
-                initialcheckboxlist.append(["data 1", "data 2", "data 3"])
-            if values['-S-'] == True:
-                initialcheckboxlist.append(["data 4"])
-            if values['-NN-'] == True:
-                initialcheckboxlist.append(["data 5", "data 6", "data 7", "data 8"])
-            if values['-N-'] == True:
-                initialcheckboxlist.append(["data 9", "data 10", "data 11", "data 12"])
-            if values['-O-'] == True:
-                initialcheckboxlist.append(["data 13", "data 14"])
-            if values['-AT-'] == True:
-                initialcheckboxlist.append(["data 15"])
-            if values['-CA-'] == True:
-                initialcheckboxlist.append(["data 16", "data 17", "data 18", "data 19", "data 20", "data 21", "data 22", "data 23"])
-            if values['-AH-'] == True:
-                initialcheckboxlist.append(["data 24"])
-            if values['-KT-'] == True:
-                initialcheckboxlist.append(["data 25"])
-            if values['-BO-'] == True:
-                initialcheckboxlist.append(["data 26", "data 27"])
-            if values['-ORPH-'] == True:
-                initialcheckboxlist.append(["data 99"])
-
-            initialcheckboxlist = [j for i in initialcheckboxlist for j in i]
-            if len(initialcheckboxlist) == 0:
-                window['assaystatus'].print("Please pick at least one fragment class (Assay Tab).")
-                
-            #Filters out the data tabs you'd like to exclude from the assaycheckboxlist
-            if len(values['-excl-']) != 0:
-                exclinput = values['-excl-']
-                
-                #Converts user inputs into the tab names
-                excl = ["data " + str(xin) for xin in exclinput]
-                
-                #Format for this line is [expression for item in iterable if condition == True]
-                assaycheckboxlist = []
-                assaycheckboxlist = [x1 for x1 in initialcheckboxlist if not any(x2 == x1 for x2 in excl)]
-                
-                
+            
+            # Processes user input
+            assaycheckboxlist = uinput()
                 
             #Filters One Ring tabs and joins them into one table based on fragment class selections, creates metadata columns
-            assayfullsheet = pd.concat(oner, keys=assaycheckboxlist)
-            assayslice = assayfullsheet.loc[:, [
-                'Class',
-                'Molecule Name',
-                'Batch Name',
-                'Batch External Identifier',
-                'Storage 96W or Box ID',
-                'Well',
-                'Concentration (mM)',
-                '384W ML',
-                '384W ML Well',
-                '1536W ML',
-                '1536W ML Well',
-                '1536W LL',
-                '1536W LL Well',
-                '1536W AP',
-                '1536W AP Well',
-                '384W AP',
-                '384W AP Well']]
-            assayslice['Target'] = values['-target-']
-            assayslice['Run Date'] = values['-rundate-']
-            assayslice['Run ID'] = values['-runid-']
-            assayslice['xb ID'] = values['-xbid-']
-            assayslice['Assay ID'] = values['-assayid-']
-            assayslice['Concentration (uM)'] = values['-conc-']
+            assayslice = dfilter(oner, assaycheckboxlist)
             
             #Prepares plate summary sheet
-            
-            apnames = []
-            if values['-A1536-'] == True:
-                assayslice = assayslice[(~assayslice['Molecule Name'].isin(['xb', 'BB8900', 'EC57', 'EC98']))]
-                datalist, EC98sdata, losdata, hiadata, loadata, zdata, windowdata = normalize1536(foldername)
-                assayslice['% Activity (DMSO) plate'] = datalist
-                apnamestemp = []
-                for tab in assaycheckboxlist:
-                    apnamestemp.append(oner[tab].loc[0, ['1536W AP']])
-                for series in apnamestemp:
-                    apnames.append(series[0])
-                assayslice = assayslice[(assayslice['Class'] != 'empty')] #Cleans up DB upload sheet
-            if values['-A384-'] == True:
-                assayslice = assayslice[(~assayslice['Molecule Name'].isin(['xb', 'BB8900', 'EC57', 'EC98']))]
-                datalist, hisdata, losdata, hiadata, loadata, zdata, windowdata = normalize384(foldername)
-                assayslice['% Activity (DMSO) plate'] = datalist
-                for tab in assaycheckboxlist:
-                    apnamestemp.append(oner[tab].loc[0, ['384W AP']])
-                for series in apnamestemp:
-                    apnames.append(series[0])
-                assayslice = assayslice[(assayslice['Class'] != 'empty')] #Cleans up DB upload sheet
+            assayslice, apnames, datalist, hisdata, losdata, hiadata, loadata, zdata, windowdata = psummary(assayslice)
                 
             #Creates the plate summary xlsx file
-            psheaders = ["Plate Name","Fold Window","Z'","High Avg.","High Std. Dev.","Low Avg.","Low Std. Dev."]
-            ps = xw.Book()
-            pssheet = ps.sheets[0]
-            pssheet.range('A1').value = psheaders
-            pssheet.range('A2').options(transpose=True).value = apnames
-            pssheet.range('B2').options(transpose=True).value = windowdata
-            pssheet.range('C2').options(transpose=True).value = zdata
-            pssheet.range('D2').options(transpose=True).value = hiadata
-            pssheet.range('E2').options(transpose=True).value = hisdata
-            pssheet.range('F2').options(transpose=True).value = loadata
-            pssheet.range('G2').options(transpose=True).value = losdata         
+            ps = pexcel()
             
             #Prepare to export the .csv and .xlsx files            
             if values['-A1536-'] == True:
